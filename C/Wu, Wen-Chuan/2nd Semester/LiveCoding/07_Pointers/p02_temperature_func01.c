4e92fb1c0c0b2556656504bb0db8e752088a6285
KevinTsaiCoding <48882710+KevinTsaiCoding@users.noreply.github.com>
2019-04-23 20:24:43 +0800

    Create p02_temperature_func01.c

---
+++ C/Wu, Wen-Chuan/2nd Semester/LiveCoding/07_Pointers/p02_temperature_func01.c
@@ -0,0 +1,19 @@
+#include<stdlib.h>
+#include<stdio.h>
+float centigradeTOfahrenheit(float *cPtr);
+int main()
+{
+	printf("Centigrade\tFahrenheit");
+	float i;
+	for(i=0.0;i<=100.0;i++){
+		printf("\n%.2f\t%.2f\n",i,centigradeTOfahrenheit(&i));
+	}
+	system("pause");
+  return 0;
+}
+float centigradeTOfahrenheit(float *cPtr)
+{
+	float f;
+	f=((*cPtr)*9/5)+32;
+	return f;	
+}
