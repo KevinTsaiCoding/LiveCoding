82cfebc55938b55ab0dc4a010e53ae5c4187c8a5
KevinTsaiCoding <48882710+KevinTsaiCoding@users.noreply.github.com>
2019-04-23 20:25:24 +0800

    Create p02_temperature_func02.c

---
+++ C/Wu, Wen-Chuan/2nd Semester/LiveCoding/07_Pointers/p02_temperature_func02.c
@@ -0,0 +1,36 @@
+#include<stdlib.h>
+#include<stdio.h>
+void CtoF(float *Ptr);
+void FtoC(float *Ptr);
+int main()
+{
+	printf("select 1 or 2:\n");
+	printf("1:攝氏轉華氏\n2:華氏轉攝氏\n");
+	int choice;
+	printf("ans:");
+	scanf("%d",&choice);
+	float temperature;
+	if(choice==1){
+		printf("temp:");
+		scanf("%f",&temperature);
+		CtoF(&temperature);
+		printf("temp= %.2f\n",temperature);
+	}
+	else if(choice==2){
+		printf("temp:");
+		scanf("%f",&temperature);
+		FtoC(&temperature);
+		printf("temp= %.2f\n",temperature);
+	}
+	system("pause");
+    return 0;
+}
+void CtoF(float *Ptr)
+{
+	*Ptr=*Ptr*9/5+32;
+	return;
+}
+void FtoC(float *Ptr)
+{
+	*Ptr=(*Ptr-32)*5/9;
+}
