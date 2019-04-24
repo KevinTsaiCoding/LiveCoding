ed6f70aefa2d430a5bf8f21b2415e245c7d2c703
KevinTsaiCoding <48882710+KevinTsaiCoding@users.noreply.github.com>
2019-04-24 17:01:13 +0800

    Update and rename addnum_01.c to p01_arithmetic_01.c

---
+++ C/Wu, Wen-Chuan/1st Semester/01_Basic_io/p01_arithmetic_01.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void){
+	printf("%d+%d = %d\n",2,2,2+2);
+	printf("%d*%d = %d\n", 2,2,2*2);
+	printf("%d-%d = %d\n", 2,2,2-2);
+	printf("%d/%d = %f\n", 2,2,(float)2/2);
+	system("pause");
+	return 0;
+} 
