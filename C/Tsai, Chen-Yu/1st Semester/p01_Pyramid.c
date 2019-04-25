7587262e4a2ad2b5af94e703e884a3a2f1f1c834
KevinTsaiCoding <48882710+KevinTsaiCoding@users.noreply.github.com>
2019-04-25 09:48:13 +0800

    Create p01_Pyramid.c

---
+++ C/Tsai, Chen-Yu/1st Semester/p01_Pyramid.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+/*AI072162*/
+	printf("     *\n");
+	printf("    ***\n");
+	printf("   *****\n");
+	printf("  *******\n");
+	printf(" *********\n");
+	printf("***********\n");
+	
+	return;
+} 
