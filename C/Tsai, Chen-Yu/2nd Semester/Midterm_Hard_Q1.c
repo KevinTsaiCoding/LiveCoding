e49f4075748a1cf5754cdebef8d1c9e795a4075c
KevinTsaiCoding <48882710+KevinTsaiCoding@users.noreply.github.com>
2019-04-25 23:58:53 +0800

    Update Midterm_Hard_Q1.c

--- C/Tsai, Chen-Yu/2nd Semester/Midterm_Hard_Q1.c
+++ C/Tsai, Chen-Yu/2nd Semester/Midterm_Hard_Q1.c
@@ -2,44 +2,42 @@
 #include<stdlib.h>
 #include<string.h>
 #define N 100
-void Encrypted(char *str,int H,int W);
+void Encrypted(char *sPtr,int H,int W);
 int main()
 {
 	char str[N];
-	printf("Input:");
+	printf("input:");
 	fgets(str,N,stdin);
-	int length=strlen(str);
 	int W;
-	printf("input :");
+	printf("input 列:");
 	scanf("%d",&W);
 	int H;
+	int length=strlen(str);
 	(length%W==0)?(H=length/W):(H=length/W+1);
-	printf("Size: %d %d",W,H);
-	puts("");
+	printf("Size: %d %d\n",H,W);
 	int last=length-1;
 	if(str[last]=='\n')
-		str[last]='\0';
+		str[last]='\0'; /* 表示 '\0' 那格 */
 	Encrypted(str,H,W);
 	system("pause");
-	return EXIT_SUCCESS;
+	return 0;
 }
-void Encrypted(char *str,int H,int W)
+void Encrypted(char *sPtr,int H,int W)
 {
-	char pwd[100][100]={0};
+	char password[100][100];
 	int i,j,nowPos=0;
-	for(i=0;i<H;i++){
-		for(j=0;j<W;j++){
-			pwd[i][j]=str[nowPos];
-			nowPos++; 
-		}
-	}
 	for(i=0;i<W;i++){
 		for(j=0;j<H;j++){
-			if(pwd[j][i]=='\0')
-				continue;
-			printf("%c",pwd[j][i]);
+			password[i][j]=sPtr[nowPos];
+			++nowPos;
 		}
 	}
-	puts("");
-	return;
+	for(i=0;i<H;i++){
+		for(j=0;j<W;j++){
+			if(password[j][i]=='\0')
+				continue;
+			printf("%c",password[j][i]);
+		}
+	}
+	printf("\n");
 }
