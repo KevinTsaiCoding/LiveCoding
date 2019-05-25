b58307bf0934880f5e00cd07a8b165043741cd28
KevinTsaiCoding <48882710+KevinTsaiCoding@users.noreply.github.com>
2019-05-25 19:40:32 +0800

    Update and rename 07_hw_demo.c to 07_Bank_Account.c

--- C/Wu, Wen-Chuan/2nd Semester/Homeworks/07_Bank_Account.c
+++ C/Wu, Wen-Chuan/2nd Semester/Homeworks/07_Bank_Account.c
@@ -77,7 +77,7 @@ void touch()
 		printf("> ");
 		scanf("%d",&data.accountNum);
 		puts("Enter lastname, firstname, and your balance");
-		scanf("%s%s%lf",data.LastName,data.FirstName,&data.balance);
+		scanf("%s%s%f",data.LastName,data.FirstName,&data.balance);
 		fseek(f1Binary,(data.accountNum-1/*cuz this data needs to write before it no including it*/)*sizeof(clientData),SEEK_SET);/* write data in here */
 		fwrite(&data,sizeof(clientData)*1,1,f1Binary);
 		fclose(f1Binary);
@@ -85,9 +85,47 @@ void touch()
 }
 void ls(void)
 {
-	
+	clientData data={0,"","",0.0};
+	FILE *f1Binary;
+	f1Binary=fopen("data.txt","rb");
+	if(f1Binary==NULL){
+		printf("No file\n");
+		exit(1);
+	}
+	else{
+		int i;
+		for(i=1;i<=100;i++)/* you now can have 100 data of the client with binary*/
+			fwrite(&data,sizeof(clientData)*1,1,f1Binary);
+		printf("Enter an account number(1 to 100, 0 to end input)\n");
+		printf("> ");
+		scanf("%d",&data.accountNum);
+		if(data.accountNum!=0){
+			fseek(f1Binary,(data.accountNum-1/*cuz this data needs to write before it no including it*/)*sizeof(clientData),SEEK_SET);/* write data in here */
+			fread(&data,sizeof(clientData)*1,1,f1Binary);
+			printf("%d %8s %8s %8.2f\n",data.accountNum,data.LastName,data.FirstName,data.balance);
+		}
+		
+		
+		fclose(f1Binary);
+	}
 }
 void rm(void)
 {
-	
+	clientData data={0,"","",0.0};
+	FILE *f1Binary;
+	f1Binary=fopen("data.txt","wb");
+	if(f1Binary==NULL){
+		printf("No file\n");
+		exit(1);
+	}
+	else{
+		printf("Enter an account number(1 to 100, 0 to end input)\n");
+		printf("> ");
+		scanf("%d",&data.accountNum);
+		if(data.accountNum!=0){
+			fseek(f1Binary,(data.accountNum-1/*cuz this data needs to write before it no including it*/)*sizeof(clientData),SEEK_SET);/* write data in here */
+			fwrite(&data,sizeof(clientData),1,f1Binary);
+		}
+		fclose(f1Binary);
+	}
 }
